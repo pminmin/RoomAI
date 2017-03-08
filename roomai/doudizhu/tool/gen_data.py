9a8faf9b74724d3722368ce7438d93b6491fe82b
lietal <lili1987mail@gmail.com>
2017-03-08 23:43:11 +0800

    gen_data completes

--- roomai/doudizhu/tool/gen_data.py
+++ roomai/doudizhu/tool/gen_data.py
@@ -5,11 +5,91 @@ from DouDiZhuPokerUtils import *
 
 actions_file            = open("actions.txt","w")
 handcards2actions_file  = open("handcards2actions.txt","w")
+AllActionsDict          = dict()
 
-AllActionsDict = dict()
+def extractStraight(hand_cards, numStraightK, count, exclude):
+    cards = []
+    count = 0
+
+    for i in xrange(ActionSpace.r-1,-1,-1):
+
+        if i not in exclude:
+            count  = 0 
+        elif hand_cards[i] >= count:
+            count += 1
+        else:
+            count  = 0        
+
+        if count >= count:
+            cards.append(hand_cards[i:i+numStraightK])
+
+
+        return cards          
+
+    
+def extractDiscrete(hand_cards, numDiscreteK, count, exclude):
+    cards   = []
+    
+    for i in xrange(numDiscrete):
+        
+        old_cards = copy.deepcopy(cards)
+        for c in xrange(len(hand_cards)):
+            if (hand_cards[c] >= count) and (c not in cards) and (c not in exclude):
+                for origin in old_cards:
+                    if len(origin) == numDiscreteK:  continue
+                    copy1 = copy.deepcopy(origin)
+                    copy1.append(c)
+                    cards.append(copy1)        
+                cards.append([c])
+
+    return cards
+
 
 def gen_actions(hand_cards):
+
+    if "i_" in pattern[0]:
+         return []    
+
+    actions = [];
+    if pattern[0] == "x_rocket":
+         if  hand_cards[ActionSpace.r] == 1 and \
+             hand_cards[ActionSpace.R] == 1:
+         action = Action([ActionSpace.r, ActionSpace.R],[])
+         actions.append(action)
+         return actions       
+
+    numMaster   = pattern[1]
+    numMasterV  = pattern[2]
+    isStraight  = pattern[3]
+    numSlave    = pattern[4]
+    numSlaveV   = pattern[5]
     
+    numMasterCount  = numMaster/numMasterV
+    numSlaveCount   = numSlave /numSlaveV
+
+    if isStraight == 1:
+        ## master Card set set
+        mCardss = extractStraight(hand_cards, numMasterV, numMasterCount, [])
+    else:
+        mCards = extractDiscrete(hand_cards, numMasterV, numMasterCount, [])
+
+    for mCards in mCardss:
+        m = []
+        for mc in mCards:
+            m.extend([mc for i in xrange(numMasterCount)])
+        m.sort()
+
+        sCardss = extractDiscrete(hand_cards, numSlaveV, numSlaveCount, mCards)
+        if len(sCardss) > 0:
+            for sCards in sCardss:
+                s = []
+                for sc in sCards:
+                    s.extend([sc for i in xrange(numSlaveCount)])
+                actions.append(Action(copy.deepcopy(m), s.sort()))
+                            
+    
+    return actions
+
 
 def gen_handcards_dfs(hand_cards, currentP, currentNum, numList):
 
