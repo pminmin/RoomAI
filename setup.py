9e9f9e5e2d1f770ad3f062c01b8ee3c7b773805c
lietal <lili1987mail@gmail.com>
2017-05-25 23:28:59 +0800

    0.1.0 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(  name        = "roomai",
-        version     = "0.1a11",
+        version     = "0.1.0",
         description = "A toolkit for developing and comparing imperfect information game bots",
         url         = "https://github.com/roomai/RoomAI",
         author      = "roomai dev",
